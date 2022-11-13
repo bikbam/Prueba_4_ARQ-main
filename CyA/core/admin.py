fe6cd0b76688ae31234345464df553132e6fecd5
jordan bolbaran <bikbokzz.78@gmail.com>
2022-11-13 17:29:56 -0300

    listo

--- CyA/core/admin.py
+++ CyA/core/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import EmpresaPyme, Producto,Categoria,Cliente
+from .models import EmpresaPyme, Producto,Categoria,Cliente,registro,venta,delivery,boleta,pagoPyme,comentarioProducto
 # Register your models here.
 
 
 admin.site.register(EmpresaPyme)
 admin.site.register(Categoria)
 admin.site.register(Producto)
+admin.site.register(Cliente)
+admin.site.register(registro)
+admin.site.register(venta)
+admin.site.register(delivery)
+admin.site.register(boleta)
+admin.site.register(pagoPyme)
+admin.site.register(comentarioProducto)
 #class ProductoAdmin(admin.ModelAdmin):
 #    list_display = ["nombreProducto","descripcionProducto","categoriaProducto","activoProducto","PrecioProducto",'stock Procuto']
 
